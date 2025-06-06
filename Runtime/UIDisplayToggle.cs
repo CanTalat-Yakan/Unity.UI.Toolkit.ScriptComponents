f1b381440350691d22480b342c68d57541210c2a
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-06 04:01:26 +0200

    ⠀⠀⠀⠀⠀

---
+++ Runtime/UIDisplayToggle.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UnityEssentials
+{
+    public class UIDisplayToggle : BaseScriptComponent<VisualElement>
+    {
+        [SerializeField] private bool _displayOnStart = true;
+
+        public void Start()
+        {
+            gameObject.SetActive(_displayOnStart);
+
+            if (gameObject.activeSelf)
+                SetEnabled(true);
+        }
+
+        public void OnEnable() =>
+            SetEnabled(true);
+
+        public void OnDisable() =>
+            SetEnabled(false);
+
+        public void SetDisplayOnStart(bool displayOnStart) =>
+            _displayOnStart = displayOnStart;
+
+        public void SetEnabled(bool enable)
+        {
+            if (!HasElements)
+                return;
+
+            IterateLinkedElements(e => e.SetDisplayEnabled(enable));
+        }
+
+        public bool GetEnabled(int index = 0)
+        {
+            if (!HasElements || LinkedElements.Length <= index)
+                return false;
+
+            return LinkedElements[index].GetDisplayEnabled();
+        }
+
+        public void Toggle() =>
+            IterateLinkedElements((element) =>
+            {
+                bool enabled = element.GetDisplayEnabled();
+                element.SetDisplayEnabled(!enabled);
+            });
+    }
+}
